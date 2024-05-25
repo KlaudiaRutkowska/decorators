87975ab7990b061c24e7f7a9cb4f0540f9c17cca
Klaudia Rutkowska <rutkowskaklaudia731@gmail.com>
2024-05-25 19:01:40 +0200

    fix exercise 2

--- src/exercise2/MainShop.java
+++ src/exercise2/MainShop.java
@@ -4,7 +4,7 @@ public class MainShop {
     public static void main(String[] args) {
         Cart cart = new Cart();
 
-        cart = new DiscountDecorator(new ShippingCostDecorator(new MascotDecorator(new LanyardDecorator(new CupDecorator(new NotebookDecorator(new PenDecorator(cart)))))));
+        cart = new ShippingCostDecorator(new DiscountDecorator(new MascotDecorator(new LanyardDecorator(new CupDecorator(new NotebookDecorator(new PenDecorator(cart)))))));
 
         cart.displayCart();
     }
